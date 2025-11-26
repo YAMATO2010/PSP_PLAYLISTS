ac6acd7ea03a7c060cd6d99ab1b98edbd105bfe0
YAMATO <191845361+Yamato2010@users.noreply.github.com>
2025-11-26 15:45:21 +0900

    Update PlayListsPSP.kts

--- PlayListsPSP.kts
+++ PlayListsPSP.kts
@@ -1,78 +1,71 @@
 
 import java.io.File
 
-main(args)
 
-fun main(args: Array<String>) {
+run{
     try {
 
+            if (args.isEmpty()) {
 
-        if (args.isEmpty()) {
-            println("使い方: kotlin <このスクリプトのパス> <ファイル名>")
-            return
-        }
-        println("今から処理を始めます")
+                println("使い方: kotlin <このスクリプトのパス> <ファイル名>")
+        
+                return@run
+            }
+            println("今から処理を始めます")
+            if (!(args[0].endsWith(".m3u8") || args[0].endsWith(".txt"))) {
 
-        if (!(args[0].endsWith(".m3u8") || args[0].endsWith(".txt"))) {
-            return
-        }
+                return@run
 
-
-        val playLIst = File(args[0])
-
-        val text: List<String> = playLIst.readLines()
-        val outputTextlist = text.map { line ->
-            val trimmed = line.trim('"')
-    val converted = if (trimmed.startsWith("/MUSIC")) {
-        trimmed
-    } else {
-        val noDrive = trimmed.replace(Regex("^[A-Z]:[\\\\¥]?"), "")
-        "/" + noDrive.replace("\\", "/").replace("¥", "/")
-    }
-    println("処理中だお: $converted")
-    converted 
-            
-
-        }
-
-
-        val outputText = outputTextlist.joinToString("\n")
-        println("変換は終わったお" + "<" + outputText + ">")
-
-
-
-
-        if (args[0].endsWith(".m3u8")) {
-
-
-            playLIst.writeText(outputText)
-            println("かきこめたお")
-
-
-        } else if (args[0].endsWith(".txt")) {
-            var Fileexistsindex = 0
-            var FileexistsindexString = ""
-
-
-            val Filename = playLIst.nameWithoutExtension
-            var newm3u8 = File(Filename + FileexistsindexString + ".m3u8")
-
-            while (newm3u8.exists()) {
-                Fileexistsindex += 1
-                FileexistsindexString = Fileexistsindex.toString()
             }
 
+            val playLIst = File(args[0])
+            val text: List<String> = playLIst.readLines()
+            val outputTextlist = text.map { line ->
 
-            newm3u8.writeText(outputText)
-            println("かきこめたお")
+            val trimmed = line.trim('"')
+            val converted = if (trimmed.startsWith("/MUSIC")) {
 
+                trimmed
+               
+            } else {
 
+                val noDrive = trimmed.replace(Regex("^[A-Z]:[\\\\¥]?"), "")
+                "/" + noDrive.replace("\\", "/").replace("¥", "/")
+
+            }
+            println("処理中だお: $converted")
+            converted 
+            }
+            val outputText = outputTextlist.joinToString("\n")
+            println("変換は終わったお" + "<" + outputText + ">")
+            if (args[0].endsWith(".m3u8")) {
+
+                playLIst.writeText(outputText)
+
+               
+                println("かきこめたお")
+            } else if (args[0].endsWith(".txt")) {
+         
+                    
+               
+                val Filename = playLIst.nameWithoutExtension
+                val Filepath = playLIst.parent
+                var newm3u8 = File(Filepath,Filename + ".m3u8")
+            
+
+                newm3u8.createNewFile()
+                
+                newm3u8.writeText(outputText)
+           
+                
+                
+                println("かきこめたお")   
+            }
+               
+                return@run
+
+            }catch (e: Exception){       
+                println("エラーだお" + "\n" + e )         
+                return@run
+            }
         }
-
-        return
-
-    }catch (e: Exception){
-        println("エラーだお" + "\n" + e )
-        return
-    }
-}
